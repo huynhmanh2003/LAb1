a48ce2fbd231b994475c7ace36cbdc6f5504ca89
huynhmanh2003 <huynhvanmanh2003@gmail.com>
2024-05-17 11:20:33 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+import HelloWorld from"./hellow.js";
+ReactDOM.render(< HelloWorld/>, document.getElementById("root"))
